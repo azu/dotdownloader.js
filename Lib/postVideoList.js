358126c6f37de47bab66daef361047e6c3ca668e
azu <azuciao@gmail.com>
2014-02-16 23:20:57 +0900

    integrate

---
+++ Lib/postVideoList.js
@@ -0,0 +1,38 @@
+/**
+ * Created by azu on 2014/02/16.
+ * LICENSE : MIT
+ */
+    // https://github.com/marcuswestin/WebViewJavascriptBridge
+function connectWebViewJavascriptBridge(callback) {
+    var rightBack = function (bridge) {
+        bridge.init(function (message, responseCallback) {
+            if (responseCallback) {
+                responseCallback("Right back atcha")
+            }
+        });
+        callback(bridge);
+    };
+    if (window.WebViewJavascriptBridge) {
+        rightBack(WebViewJavascriptBridge)
+    } else {
+        document.addEventListener('WebViewJavascriptBridgeReady', function () {
+            rightBack(WebViewJavascriptBridge)
+        }, false)
+    }
+}
+
+// 'isInstalledApp' : 配列 がhookキー
+function postVideoList(videoIDs, callback) {
+    /*
+    {
+    "title"
+    "url"
+    "videoID"
+    }
+     */
+    connectWebViewJavascriptBridge(function (bridge) {
+        bridge.callHandler('post-video-list', videoIDs, callback)
+    });
+}
+
+module.exports = postVideoList;
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by azu on 2014/02/16.
+ * LICENSE : MIT
+ */
+var Promise = require("bluebird");
+
+(function () {
+    var xhr = require("./Lib/xhr");
+    var aLinks = document.querySelectorAll(".lesson_title");
+    var urlObject = Array.prototype.slice.call(aLinks).map(function (ele) {
+        return {
+            "title": ele.textContent,
+            "url": ele.href
+        };
+    });
+
+    function getYoutubeID(source) {
+        var reg = /videoId: '(.*?)',/i;
+        var m = source.match(reg);
+        return m && m[1];
+    }
+
+    var promiseList = urlObject.map(function (object) {
+        return xhr(object["url"]).then(getYoutubeID).then(function (youtubeID) {
+            object.videoID = youtubeID;
+            return object;
+        });
+    });
+    Promise.all(promiseList).then(function (objects) {
+        console.log("objects", objects);
+        require("./Lib/postVideoList")(objects, function () {
+            console.log("youtubeIDs", objects);
+        });
+    });
+})();
