07277cf17399e8d9918b0f9c3af12435a40f0e27
azu <azuciao@gmail.com>
2014-02-18 22:41:52 +0900

    fix bundle

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ var Promise = require("bluebird");
 (function () {
     var xhr = require("./Lib/xhr");
     var aLinks = document.querySelectorAll(".lesson_title");
+    var documentTitle = document.title;
+    var lessonTitle = document.querySelector('meta[property="og:title"]').content || documentTitle;
     var urlObject = Array.prototype.slice.call(aLinks).map(function (ele) {
         return {
+            "lessonTitle": lessonTitle,
             "title": ele.textContent,
             "url": ele.href
         };
